e58e33163906298a335823cc04c077035124d8e0
ccvelandres <ccvelandres@gmail.com>
2022-05-15 17:14:19 +0800

    Added validation layer debug callback

---
+++ core/src/renderer/vulkan/vk-debug.cpp
@@ -0,0 +1,15 @@
+#include <vulkan/vulkan.hpp>
+
+#include <iostream>
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+    void *pUserData)
+{
+
+    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+    return VK_FALSE;
+}
