42c0865ef021b0396271c03dd4e8a639113afa72
ccvelandres <ccvelandres@gmail.com>
2022-05-15 13:20:31 +0800

    Added runtime error macros to logging

--- core/include/utils/logging.hpp
+++ core/include/utils/logging.hpp
@@ -1,18 +1,23 @@
 #pragma once
 
 #include <spdlog/spdlog.h>
+#include <exception>
 
 /** We use alias for spdlog in case we change it in the future */
-namespace logging {
+namespace logging
+{
     using namespace spdlog;
 }
 
 /** Helper macros for appending the line and function */
-#define L_TAG(STR) static const std::string tag__{STR}
+#define L_TAG(STR) \
+    static const std::string tag__ { STR }
 
 #define L_LOG(level, STR, ...) logging::level("[{}] " STR, tag__, ##__VA_ARGS__)
 #define L_ERROR(STR, ...) L_LOG(error, STR, ##__VA_ARGS__)
-#define L_WARNING(STR, ...) L_LOG(warning, STR, ##__VA_ARGS__)
+#define L_WARN(STR, ...) L_LOG(warn, STR, ##__VA_ARGS__)
 #define L_INFO(STR, ...) L_LOG(info, STR, ##__VA_ARGS__)
 #define L_DEBUG(STR, ...) L_LOG(debug, STR, ##__VA_ARGS__)
 #define L_TRACE(STR, ...) L_LOG(trace, STR, ##__VA_ARGS__)
+
+#define L_THROW_RUNTIME(STR, ...) throw std::runtime_error(fmt::format("[{}] " STR, tag__, ##__VA_ARGS__));
