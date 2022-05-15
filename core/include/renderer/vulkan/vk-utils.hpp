ad8b9fc48ea9185732c502c5fcc8d545e66ab04d
ccvelandres <ccvelandres@gmail.com>
2022-05-15 13:18:55 +0800

    Added vk-utils

---
+++ core/include/renderer/vulkan/vk-utils.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+
+struct VulkanUtils {
+    static bool checkSwapchainSupport(vk::PhysicalDevice &device);
+    static bool checkValidationLayerSupport(const char* validationLayers,
+                                            const std::vector<vk::LayerProperties> &availableLayers);
+};
