e427b442c57a1f1d58b12bb752225d4f1e6675c3
ccvelandres <ccvelandres@gmail.com>
2022-05-28 22:40:56 +0800

    Added vk-debug

---
+++ core/include/renderer/vulkan/vk-debug.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <memory>
+#include <SDL2/SDL.h>
+#include <vulkan/vulkan.hpp>
+
+class VulkanDebugUtils
+{
+private:
+    struct Internal;
+    std::unique_ptr<Internal> m_internal;
+
+protected:
+public:
+    VulkanDebugUtils( const vk::Instance &instance );
+    ~VulkanDebugUtils();
+};
--- core/src/renderer/vulkan/vk-debug.cpp
+++ core/src/renderer/vulkan/vk-debug.cpp
@@ -1,15 +1,92 @@
-#include <vulkan/vulkan.hpp>
+#include <renderer/vulkan/vk-debug.hpp>
+
+#include <utils/logging.hpp>
 
 #include <iostream>
 
-static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
-    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-    VkDebugUtilsMessageTypeFlagsEXT messageType,
-    const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
-    void *pUserData)
+static VKAPI_ATTR VkBool32 VKAPI_CALL
+debugCallback( VkDebugUtilsMessageSeverityFlagBitsEXT      messageSeverity,
+               VkDebugUtilsMessageTypeFlagsEXT             messageType,
+               const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+               void                                       *pUserData )
 {
-
-    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+    if ( messageSeverity
+         == static_cast<VkDebugUtilsMessageSeverityFlagBitsEXT>(
+             vk::DebugUtilsMessageSeverityFlagBitsEXT::eError ) )
+    {
+        logging::error( "[vk-debug]: {}", pCallbackData->pMessage );
+    }
+    else if ( messageSeverity
+              == static_cast<VkDebugUtilsMessageSeverityFlagBitsEXT>(
+                  vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning ) )
+    {
+        logging::warn( "[vk-debug]: {}", pCallbackData->pMessage );
+    }
+    else if ( messageSeverity
+              == static_cast<VkDebugUtilsMessageSeverityFlagBitsEXT>(
+                  vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo ) )
+    {
+        logging::info( "[vk-debug]: {}", pCallbackData->pMessage );
+    }
+    else if ( messageSeverity
+              == static_cast<VkDebugUtilsMessageSeverityFlagBitsEXT>(
+                  vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose ) )
+    {
+        logging::trace( "[vk-debug]: {}", pCallbackData->pMessage );
+    }
 
     return VK_FALSE;
 }
+
+vk::DebugUtilsMessengerEXT createDebugUtilsMessenger( const vk::Instance              &instance,
+                                                      const vk::DispatchLoaderDynamic &dispatcher )
+{
+    vk::DebugUtilsMessengerCreateInfoEXT
+        createInfo( vk::DebugUtilsMessengerCreateFlagsEXT(),
+                    vk::DebugUtilsMessageSeverityFlagBitsEXT::eError
+                        | vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose
+                        | vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning,
+                    vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral
+                        | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance
+                        | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation,
+                    debugCallback,
+                    nullptr );
+
+    // PFN_vkCreateDebugUtilsMessengerEXT fn = reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>(
+    //     vkGetInstanceProcAddr( instance, "vkCreateDebugUtilsMessengerEXT" ) );
+
+    // fn(instance, )
+    // auto d = vk::DispatchLoaderDynamic(instance, vkGetInstanceProcAddr);
+    // vk::DispatchLoaderDynamic d(instance, vkGetInstanceProcAddr);
+    return instance.createDebugUtilsMessengerEXT( createInfo, nullptr, dispatcher );
+}
+
+struct VulkanDebugUtils::Internal
+{
+    const vk::Instance              &instance;
+    const vk::DispatchLoaderDynamic  dispatcher;
+    const vk::DebugUtilsMessengerEXT debugUtilsMessenger;
+
+    Internal( const vk::Instance &instance )
+        : instance( instance ),
+          dispatcher( instance, vkGetInstanceProcAddr ),
+          debugUtilsMessenger( ::createDebugUtilsMessenger( instance, dispatcher ) )
+    {
+        L_TAG( "VulkanDebugUtils::Internal" );
+        L_DEBUG( "DebugUtils successful setup" );
+    }
+
+    ~Internal()
+    {
+        L_TAG( "VulkanDebugUtils::~Internal" );
+        instance.destroyDebugUtilsMessengerEXT( debugUtilsMessenger, nullptr, dispatcher );
+        L_DEBUG( "Internal data cleaned up" );
+    }
+};
+
+VulkanDebugUtils::VulkanDebugUtils( const vk::Instance &instance )
+    : m_internal( std::make_unique<Internal>( instance ) )
+{
+}
+
+VulkanDebugUtils::~VulkanDebugUtils() {}
