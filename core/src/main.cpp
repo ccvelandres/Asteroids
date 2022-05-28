5e726d0f993f2aefa301432a635062a292aeba9d
ccvelandres <ccvelandres@gmail.com>
2022-05-28 22:42:36 +0800

    Updated main.cpp

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,50 +1,48 @@
 #include <iostream>
-#include <thread>
 #include <map>
+#include <thread>
 #include <typeindex>
 
 #include <SDL.h>
 #include <SDL2_gfxPrimitives.h>
 
-#include <game.hpp>
-#include <event.hpp>
 #include <ecs/ecs.hpp>
+#include <event.hpp>
+#include <game.hpp>
 #include <utils/logging.hpp>
 
-#include <logic/player.hpp>
 #include <logic/enemy.hpp>
+#include <logic/player.hpp>
 
-constexpr int windowWidth = 1280;
+#include <renderer/vulkan/vk-renderer.hpp>
+
+constexpr int windowWidth  = 1280;
 constexpr int windowHeight = 720;
-constexpr int originX = windowWidth / 2;
-constexpr int originY = windowHeight / 2;
+constexpr int originX      = windowWidth / 2;
+constexpr int originY      = windowHeight / 2;
 
 auto app_start = std::chrono::high_resolution_clock::now();
 
-int main(int arc, char **argv)
+int main( int arc, char **argv )
 {
     /** Set compact pattern for logging */
-    logging::set_pattern("[%H:%M:%S.%e] %L : %v");
-    logging::set_level(logging::level::debug);
+    logging::set_pattern( "[%H:%M:%S.%e] %L : %v" );
+    logging::set_level( logging::level::trace );
 
-    Game *game = new Game("Asteroids", windowWidth, windowHeight);
+    Game *game = new Game( "Asteroids", windowWidth, windowHeight );
     game->init();
-    game->setTargetFPS(90);
-    
-    Player player = Game::entityManager()->addEntity<Player>();
-    EntityList<Enemy> enemies = Game::entityManager()->addEntities<Enemy>(8);
+    game->setTargetFPS( 90 );
 
-    for (int i = 0; i < enemies.size(); i++)
+    Player            player  = Game::entityManager()->addEntity<Player>();
+    EntityList<Enemy> enemies = Game::entityManager()->addEntities<Enemy>( 8 );
+
+    for ( int i = 0; i < enemies.size(); i++ )
     {
-        int type=0;
-        Enemy &e = *enemies[i].get();
-        if (i < enemies.size() / 2) {
-            
-        }
-        else {
+        int    type = 0;
+        Enemy &e    = *enemies [i].get();
+        if ( i >= enemies.size() / 2 )
             type = 1;
-        }
-        e.spawn(type, Vector3F((windowWidth / enemies.size()) * i, 50, 0));
+        e.spawn( type, Vector3F( ( windowWidth / enemies.size() ) * i, 50, 0 ) );
     }
 
     game->startGameLoop();
