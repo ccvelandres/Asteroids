b9bc1d2a8e77d8abff999a27569a91ad359525bd
ccvelandres <ccvelandres@gmail.com>
2022-06-04 13:18:00 +0800

    Added renderer/common/vertex

---
+++ core/include/renderer/common/vertex.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <utils/vector.hpp>
+
+class Vertex
+{
+public:
+    /** @todo: replace vector with maybe glm vectors */
+    Vector3F position;
+    Vector3F texCoordinates;
+};
