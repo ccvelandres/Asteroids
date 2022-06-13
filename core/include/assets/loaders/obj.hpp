8a05658c124d48d6fca6068fa40b28fc84bab86b
ccvelandres <ccvelandres@gmail.com>
2022-06-13 10:49:46 +0800

    Working asset::loader for obj files

--- core/include/assets/loaders/obj.hpp
+++ core/include/assets/loaders/obj.hpp
@@ -2,45 +2,43 @@
 
 #include <string>
 
-namespace assets::loaders
+namespace assets::loaders::obj
 {
-    class OBJLoader
+    struct Indices
+    {
+        int v;
+        int vt;
+        int vn;
+
+        Indices() : v(-1), vt(-1), vn(-1) {}
+        Indices(int idx) : v(idx), vt(idx), vn(idx) {}
+        Indices(int vidx, int vtidx, int vnidx) : v(vidx), vt(vtidx), vn(vnidx) {}
+    };
+
+    struct Mesh
+    {
+        std::vector<Indices> indices;      /** indices for each face */
+        std::vector<int>     verticeCount; /** vertice count per face */
+    };
+
+    struct Object {
+        std::string name;
+        Mesh mesh;
+    };
+
+    class Loader
     {
     public:
-        struct Indices
-        {
-            int v;
-            int vt;
-            int vn;
-
-            Indices() : v(-1), vt(-1), vn(-1) {}
-            Indices(int idx) : v(idx), vt(idx), vn(idx) {}
-            Indices(int vidx, int vtidx, int vnidx) : v(vidx), vt(vtidx), vn(vnidx) {}
-        };
-
-        struct Mesh
-        {
-            std::vector<Indices> indices;      /** indices for each face */
-            std::vector<int>     verticeCount; /** vertice count per face */
-        };
-
-        struct Object {
-            std::string name;
-            Mesh mesh;
-        };
     private:
-        struct Face;
-
         std::vector<float> m_vertices;
         std::vector<float> m_normals;
         std::vector<float> m_texCoordinates;
 
         std::vector<Object> m_objects;
 
-        // void pushObject(std::vector<Indices> &indices);
-        void parse(std::istream &s);
+        bool parse(std::istream &s) noexcept;
     public:
-        OBJLoader();
+        Loader();
 
         bool loadFile(const std::string &filename);
     };
