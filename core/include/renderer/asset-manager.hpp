35c69436e5a9cd436fef02e38f9f886d105e9911
ccvelandres <ccvelandres@gmail.com>
2022-07-04 21:32:43 +0800

    Add AssetManager virtual class

---
+++ core/include/renderer/asset-manager.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <assets/asset-inventory.hpp>
+
+using AssetID = std::size_t;
+
+class AssetManager {
+private:
+protected:
+public:
+
+    virtual AssetID loadPipeline(const std::string &name) = 0;
+    virtual AssetID loadMesh(const std::string &name) = 0;
+    virtual AssetID loadTexture(const std::string &name) = 0;
+};
