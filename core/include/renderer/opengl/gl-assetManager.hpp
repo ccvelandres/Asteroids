dec29cf543f147e02d231efd0ed7904c4a2d8219
ccvelandres <ccvelandres@gmail.com>
2022-07-04 21:49:18 +0800

    Added OpenGLAssetManager

---
+++ core/include/renderer/opengl/gl-assetManager.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <renderer/asset-manager.hpp>
+#include "gl-renderer.hpp"
+#include "gl-pipeline.hpp"
+#include "gl-texture.hpp"
+#include "gl-mesh.hpp"
+
+class OpenGLAssetManager : public AssetManager
+{
+private:
+    struct Internal;
+    std::unique_ptr<Internal> m_internal;
+protected:
+public:
+    OpenGLAssetManager();
+    OpenGLAssetManager(OpenGLAssetManager &&o);
+    OpenGLAssetManager &operator=(OpenGLAssetManager &&o);
+    ~OpenGLAssetManager();
+
+    virtual AssetID loadPipeline(const std::string &name) override;
+    virtual AssetID loadMesh(const std::string &name) override;
+    virtual AssetID loadTexture(const std::string &name) override;
+
+    OpenGLMesh     &getMesh(AssetID id);
+    OpenGLPipeline &getPipeline(AssetID id);
+    OpenGLTexture  &getTexture(AssetID id);
+};
---
+++ core/src/renderer/opengl/gl-assetManager.cpp
@@ -0,0 +1,117 @@
+#include <renderer/opengl/gl-assetManager.hpp>
+#include <utils/logging.hpp>
+
+#include <unordered_map>
+
+struct OpenGLAssetManager::Internal
+{
+    /** @todo: probably better to replace this with vector? but need to ensure index-value should not change ever */
+    std::unordered_map<AssetID, OpenGLMesh>     meshCache;
+    std::unordered_map<AssetID, OpenGLPipeline> pipelineCache;
+    std::unordered_map<AssetID, OpenGLTexture>  textureCache;
+
+    Internal()
+    {
+        L_TAG("OpenGLAssetManager::Internal");
+        L_TRACE("Internal resources initialized ({})", static_cast<void *>(this));
+    }
+
+    ~Internal()
+    {
+        L_TAG("OpenGLAssetManager::~Internal");
+        L_TRACE("Internal resources freed ({})", static_cast<void *>(this));
+    }
+};
+
+AssetID OpenGLAssetManager::loadPipeline(const std::string &name)
+{
+    L_TAG("OpenGLAssetManager::loadPipeline");
+    auto &cache = m_internal->pipelineCache;
+    AssetID id = cache.size();
+
+    /** Get paths from asset manager */
+    AssetInventory::AssetPaths assetPaths = AssetInventory::resolvePath("pipeline", name);
+
+    L_DEBUG("Loading shader pipeline \"{}\" with {} stages", name, assetPaths.size());
+    std::vector<OpenGLPipeline::ShaderStage> stages;
+
+    /** Need to indentify type of shader based from name */
+    for (auto &stagePath : assetPaths)
+    {
+        std::string shaderExt = stagePath.substr(stagePath.find_last_of("."));
+        GLenum      type      = OpenGLPipeline::shaderTypeFromName(shaderExt);
+        stages.push_back(OpenGLPipeline::ShaderStage(type, stagePath));
+    }
+
+    cache.insert(std::make_pair(id, OpenGLPipeline(name, stages)));
+    L_DEBUG("Pipeline created {}: {}", id, name);
+    return id;
+}
+
+AssetID OpenGLAssetManager::loadMesh(const std::string &name)
+{
+    L_TAG("OpenGLAssetManager::loadMesh");
+    auto   &cache = m_internal->meshCache;
+    AssetID id    = cache.size();
+
+    /** Get paths from asset manager */
+    L_DEBUG("Loading mesh from {}", name);
+    AssetInventory::AssetPaths assetPaths = AssetInventory::resolvePath("mesh", name);
+
+    /** Parse mesh file */
+    assert(assetPaths.size() == 1);
+    assets::Mesh mesh(assetPaths[0]);
+
+    cache.insert(std::make_pair(id, OpenGLMesh(mesh)));
+    L_DEBUG("Mesh loaded {}: {}", id, name);
+    return id;
+}
+AssetID OpenGLAssetManager::loadTexture(const std::string &name)
+{
+    L_TAG("OpenGLAssetManager::loadMesh");
+    auto   &cache = m_internal->meshCache;
+    AssetID id    = cache.size();
+
+    /** Get paths from asset manager */
+    AssetInventory::AssetPaths assetPaths = AssetInventory::resolvePath("texture", name);
+
+    return id;
+}
+
+OpenGLMesh &OpenGLAssetManager::getMesh(AssetID id)
+{
+    L_TAG("OpenGLAssetManager::getMesh");
+    auto &cache = m_internal->meshCache;
+    auto  it    = cache.find(id);
+    if (it != cache.find(id))
+        return (*it).second;
+    else
+        L_THROW_RUNTIME("Cache empty for AssetID: {}", id);
+}
+
+OpenGLPipeline &OpenGLAssetManager::getPipeline(AssetID id)
+{
+    L_TAG("OpenGLAssetManager::getPipeline");
+    auto &cache = m_internal->pipelineCache;
+    auto  it    = cache.find(id);
+    if (it != cache.find(id))
+        return (*it).second;
+    else
+        L_THROW_RUNTIME("Cache empty for AssetID: {}", id);
+}
+
+OpenGLTexture &OpenGLAssetManager::getTexture(AssetID id)
+{
+    L_TAG("OpenGLAssetManager::getTexture");
+    auto &cache = m_internal->textureCache;
+    auto  it    = cache.find(id);
+    if (it != cache.find(id))
+        return (*it).second;
+    else
+        L_THROW_RUNTIME("Cache empty for AssetID: {}", id);
+}
+
+OpenGLAssetManager::OpenGLAssetManager() : m_internal(std::make_unique<Internal>()) {}
+OpenGLAssetManager::OpenGLAssetManager(OpenGLAssetManager &&o)            = default;
+OpenGLAssetManager &OpenGLAssetManager::operator=(OpenGLAssetManager &&o) = default;
+OpenGLAssetManager::~OpenGLAssetManager()                                 = default;
