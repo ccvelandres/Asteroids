6065b36c5d6a1badfd7f173f697433963a535049
ccvelandres <ccvelandres@gmail.com>
2022-07-19 19:48:21 +0800

    core: renderer: gl-renderer: update render implementation

--- core/src/renderer/opengl/gl-renderer.cpp
+++ core/src/renderer/opengl/gl-renderer.cpp
@@ -4,6 +4,7 @@
 #include <renderer/opengl/gl-assetManager.hpp>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
 
 #include <utils/logging.hpp>
 
@@ -24,16 +25,13 @@ static void initGL()
     {
         L_THROW_RUNTIME("Failed to initialize glew");
     }
-
-    // if (! glewIsSupported("GL_VERSION_4_6"))
-    // {
-    //     L_THROW_RUNTIME( "Required OpenGL extensions missing" );
-    // }
 }
 
 SDL_Window *createWindow(const std::string &windowTitle, const int windowWidth, const int windowHeight)
 {
     L_TAG("createWindow");
+
+    L_DEBUG("SDL VideoDriver: {}", SDL_GetCurrentVideoDriver());
 
     SDL_Window *window = SDL_CreateWindow(windowTitle.c_str(),
                                           SDL_WINDOWPOS_CENTERED,
@@ -58,10 +56,29 @@ SDL_GLContext createContext(SDL_Window *window)
 
     initGL();
 
+    /** Set Vsync */
+    L_ASSERT(SDL_GL_SetSwapInterval(1) == 0, "Could not set Vsync");
+
     int viewportWidth, viewportHeight;
     SDL_GL_GetDrawableSize(window, &viewportWidth, &viewportHeight);
-
     L_TRACE("OpenGL Context created with viewport size: {}, {}", viewportHeight, viewportWidth);
+
+    GLenum error;
+    // Initialize Projection Matrix
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    error = glGetError();
+    L_ASSERT(error == GL_NO_ERROR,
+             "Error initializing Projection Matrix: {}",
+             reinterpret_cast<const char *>(glewGetErrorString(error)));
+
+    // Initialize Modelview Matrix
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    error = glGetError();
+    L_ASSERT(error == GL_NO_ERROR,
+             "Error initializing Modelview Matrix: {}",
+             reinterpret_cast<const char *>(glewGetErrorString(error)));
 
     glClearDepth(1.0f);
     glEnable(GL_DEPTH_TEST);
@@ -74,8 +91,8 @@ SDL_GLContext createContext(SDL_Window *window)
 
 struct OpenGLRenderer::Internal
 {
-    SDL_Window *const window;
-    SDL_GLContext     context;
+    SDL_Window *const  window;
+    SDL_GLContext      context;
     OpenGLAssetManager assetManager;
 
     Internal(const std::string &windowTitle, const int windowWidth, const int windowHeight)
@@ -111,10 +128,48 @@ void OpenGLRenderer::clean() {}
 
 void OpenGLRenderer::refresh() {}
 
-bool OpenGLRenderer::renderBegin() { return true; }
+bool OpenGLRenderer::renderBegin()
+{
+    // SDL_GL_MakeCurrent(m_internal->window, m_internal->context);
 
-ender() {}
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-void OpenGLRenderer::renderEnd() {}
+    return true;
+}
 
-AssetManager& OpenGLRenderer::getAssetManager() { return m_internal->assetManager; }
+#include <core/ecs/componentManager.hpp>
+#include <core/ecs/components.hpp>
+#include <core/ecs/entity.hpp>
+#include <glm/gtx/transform.hpp>
+#include <core/camera/camera.hpp>
+#include <core/game.hpp>
+
+void OpenGLRenderer::render()
+{
+    /** @todo: this needs reworking, better rendering management for other types */
+    ComponentManager   &componentManager = ComponentManager::getInstance();
+    OpenGLAssetManager &am               = dynamic_cast<OpenGLAssetManager &>(this->getAssetManager());
+    Camera             *camera           = Game::camera();
+    OpenGLPipeline     &pipeline         = m_internal->assetManager.getPipeline(0);
+
+    ComponentList<MeshRenderer> meshComponents = componentManager.getComponents<MeshRenderer>();
+
+    glm::mat4 projectionMatrix = camera->getProjectionMatrix();
+    glm::mat4 viewMatrix       = camera->getViewMatrix();
+
+    for (auto &it : meshComponents)
+    {
+        OpenGLMesh         &mesh        = am.getMesh(it->getMeshID());
+        OpenGLTexture      &texture     = am.getTexture(it->getTextureID());
+        TransformComponent &transform   = it->entity().getComponent<TransformComponent>();
+        glm::mat4           modelMatrix = transform.getModelMatrix();
+        glm::mat4           mvp(projectionMatrix * viewMatrix * modelMatrix);
+
+        pipeline.render(mesh, mvp);
+    }
+}
+
+void OpenGLRenderer::renderEnd() { SDL_GL_SwapWindow(m_internal->window); }
+
+AssetManager &OpenGLRenderer::getAssetManager() { return m_internal->assetManager; }
