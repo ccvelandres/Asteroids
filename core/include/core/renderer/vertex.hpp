2326faa1827d20b092b4423978797ab43f9e28e5
ccvelandres <ccvelandres@gmail.com>
2022-07-19 15:49:41 +0800

    core: renderer: added hash function for Vertex

--- core/include/core/renderer/vertex.hpp
+++ core/include/core/renderer/vertex.hpp
@@ -1,11 +1,32 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <glm/gtx/hash.hpp>
 
-class Vertex
+struct Vertex
 {
-public:
     glm::vec3 position;
     glm::vec3 normals;
     glm::vec2 texCoords;
+
+    bool operator==(const Vertex &o) const
+    {
+        return this->position == o.position && this->normals == o.normals && this->texCoords == o.texCoords;
+    }
 };
+
+namespace std
+{
+    /** Create hash function for vertex */
+    template <>
+    struct hash<Vertex>
+    {
+        size_t operator()(const Vertex &o) const
+        {
+            std::size_t h_pos = hash<glm::vec3>()(o.position);
+            std::size_t h_nor = hash<glm::vec3>()(o.normals);
+            std::size_t h_tex = hash<glm::vec2>()(o.texCoords);
+            return ((h_pos ^ (h_nor << 1) >> 1) ^ (h_tex << 1) >> 1);
+        }
+    };
+}
