7436a2fd79af6b02b8e2d2bbd5a88ee4e2c8f3cb
ccvelandres <ccvelandres@gmail.com>
2022-07-20 09:07:08 +0800

    core: input: refactored input manager

--- core/include/core/input/inputManager.hpp
+++ core/include/core/input/inputManager.hpp
@@ -1,15 +1,54 @@
 #pragma once
-#include <vector>
-#include "../manager.hpp"
+
+/**
+ * @file core/input/inputManager.hpp
+ * @author Cedric Velandres (ccvelandres@gmail.com)
+ */
+
+/**
+ * @defgroup Input
+ * @brief Interface for Input system
+ * @ingroup Core
+ * @{
+ */
+
+#include "../time.hpp"
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_scancode.h>
 
+#include <vector>
+
+class InputComponent; /** Forward declaration for InputComponent */
+
+/**
+ * @brief Manager for all Input related system
+ * @todo: maybe replace SDL_Scancode with our own type
+ *
+ */
 class InputManager
 {
 private:
+    static InputManager *m_instance;
     const uint8_t       *keyState;
     std::vector<uint8_t> lastFrameKeyState;
+    std::vector<SDL_Event> inputEvents;
+
+    InputManager();
+protected:
 public:
-    InputManager() {}
+    ~InputManager();
+    InputManager(InputManager &o)             = delete;
+    InputManager(InputManager &&o)            = delete;
+    InputManager &operator=(InputManager &o)  = delete;
+    InputManager &operator=(InputManager &&o) = delete;
+
+    /**
+     * @brief Get the Instance object
+     *
+     * @return InputManager& reference to InputManager
+     */
+    static InputManager &getInstance();
+
 
     bool isPressed(const SDL_Scancode &scanCode);
     bool isTapped(const SDL_Scancode &scanCode);
@@ -17,7 +56,10 @@ public:
     void keyDown(const SDL_Scancode &scanCode);
     void keyUp(const SDL_Scancode &scanCode);
 
+
     void init();
     void update();
     void postUpdate();
 };
+
+/** @} endgroup OpenGL */
--- core/src/input/inputManager.cpp
+++ core/src/input/inputManager.cpp
@@ -1,16 +1,93 @@
-#include <input/inputManager.hpp>
-#include <cassert>
+#include <core/input/inputManager.hpp>
+#include <core/utils/logging.hpp>
 
 #include <SDL2/SDL.h>
+
+#include <unordered_map>
+
+const std::unordered_map<SDL_EventType, bool> inputEventTypes = {
+    {SDL_KEYDOWN,                  1},
+    {SDL_KEYUP,                    1},
+    {SDL_TEXTEDITING,              1},
+    {SDL_TEXTINPUT,                1},
+    {SDL_KEYMAPCHANGED,            1},
+    {SDL_MOUSEMOTION,              1},
+    {SDL_MOUSEBUTTONDOWN,          1},
+    {SDL_MOUSEBUTTONUP,            1},
+    {SDL_MOUSEWHEEL,               1},
+    {SDL_JOYAXISMOTION,            1},
+    {SDL_JOYBALLMOTION,            1},
+    {SDL_JOYHATMOTION,             1},
+    {SDL_JOYBUTTONDOWN,            1},
+    {SDL_JOYBUTTONUP,              1},
+    {SDL_JOYDEVICEADDED,           1},
+    {SDL_JOYDEVICEREMOVED,         1},
+    {SDL_CONTROLLERAXISMOTION,     1},
+    {SDL_CONTROLLERBUTTONDOWN,     1},
+    {SDL_CONTROLLERBUTTONUP,       1},
+    {SDL_CONTROLLERDEVICEADDED,    1},
+    {SDL_CONTROLLERDEVICEREMOVED,  1},
+    {SDL_CONTROLLERDEVICEREMAPPED, 1},
+    {SDL_FINGERDOWN,               1},
+    {SDL_FINGERUP,                 1},
+    {SDL_FINGERMOTION,             1},
+    {SDL_DOLLARGESTURE,            1},
+    {SDL_DOLLARRECORD,             1},
+    {SDL_MULTIGESTURE,             1},
+    {SDL_CLIPBOARDUPDATE,          1},
+    {SDL_DROPFILE,                 1},
+    {SDL_DROPTEXT,                 1},
+    {SDL_DROPBEGIN,                1},
+    {SDL_DROPCOMPLETE,             1}
+};
+
+InputManager *InputManager::m_instance = nullptr;
+
+InputManager::InputManager() = default;
+InputManager::~InputManager() { m_instance = nullptr; }
+
+InputManager &InputManager::getInstance()
+{
+    /** @todo: use CAS for assignment? */
+    if (m_instance) m_instance = new InputManager();
+    return *m_instance;
+}
 
 void InputManager::init()
 {
     int numKeys = 0;
     keyState    = SDL_GetKeyboardState(&numKeys);
     lastFrameKeyState.reserve(numKeys);
+    /** @todo: do we setup event filters here for input events? */
 }
 
-void InputManager::update() { /** @todo: if input buffered, update internal state here */ }
+void InputManager::update()
+{
+    L_TAG("InputManager::update");
+
+    /** Clear current event queue (hopefully we dont need to reserve since clear doesn't free current memory) */
+    inputEvents.clear();
+
+    SDL_FilterEvents(
+        [](void *userdata, SDL_Event *event) -> int {
+            std::vector<SDL_Event> *inputEvents = static_cast<std::vector<SDL_Event>*>(userdata);
+
+            /** @todo: should we make the map non-const and push non-filtered event types with 0 value ? */
+            auto it = inputEventTypes.find(static_cast<SDL_EventType>(event->type));
+            if (it != inputEventTypes.end() && it->second)
+            {
+                /** Push filtered input events to our the input event queue */
+                inputEvents->push_back(*event);
+                return 0;
+            }
+            return 1;
+        },
+        &inputEvents);
+
+    /** Feed the current input event queue to input components */
+    /** @todo: possible threaded optimization for input events */
+    L_TRACE("Queue has {} input events", inputEvents.size());
+}
 
 void InputManager::postUpdate() { memcpy(lastFrameKeyState.data(), keyState, lastFrameKeyState.size()); }
 
@@ -22,13 +99,9 @@ bool InputManager::isTapped(const SDL_Scancode &scanCode)
 }
 
 void InputManager::keyDown(const SDL_Scancode &scanCode)
-{
-    /** @todo Implement buffering of inputs */
-    assert(0);
+{ /** @todo Implement buffering of inputs */
 }
 
 void InputManager::keyUp(const SDL_Scancode &scanCode)
-{
-    /** @todo Implement buffering of inputs */
-    assert(0);
+{ /** @todo Implement buffering of inputs */
 }
