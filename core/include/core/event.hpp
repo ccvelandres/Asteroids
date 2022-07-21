3046383ccb900d3c00a394209d7668374b5cff75
ccvelandres <ccvelandres@gmail.com>
2022-07-21 11:21:53 +0800

    core: events: cleared implementation

--- core/include/core/event.hpp
+++ core/include/core/event.hpp
@@ -29,33 +29,8 @@ public:
         m_eventListeners[evType].push_back(eventHandler);
     }
 
-    void init()
-    {
-        SDL_SetEventFilter(
-            [](void *userdata, SDL_Event *event) -> int {
-                switch (event->type)
-                {
-                case SDL_KEYUP:
-                case SDL_KEYDOWN:
-                case SDL_TEXTEDITING:
-                case SDL_TEXTINPUT:
-                    return 0;
-                default:
-                    return 1;
-                }
-            },
-            NULL);
-    }
+    void init() {}
 
     /** Poll SDL Events */
-    void handleEvents()
-    {
-        SDL_PumpEvents();
-
-        const uint8_t *state = SDL_GetKeyboardState(NULL);
-        // if (state[SDL_SCANCODE_0])
-        //     std::cout << "Keypress 0" << std::endl;
-        // if (state[SDL_SCANCODE_1])
-        //     std::cout << "Keypress 0" << std::endl;
-    }
+    void handleEvents() {}
 };
