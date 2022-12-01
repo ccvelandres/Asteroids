4e2d9afdd882e8d8709826bf260f5f090227e84a
ccvelandres <ccvelandres@gmail.com>
2022-12-01 22:35:21 +0800

    wip audio manager

--- core/src/audio/audioManager.cpp
+++ core/src/audio/audioManager.cpp
@@ -6,7 +6,20 @@
 
 #include <vector>
 
+/** @todo: control defaults depending on config */
+constexpr int             audioDeviceAllowedChanges = 0;
+constexpr int             defaultAudioFrequency     = 48000;
+constexpr uint8_t         defaultAudioChannel       = 2;
+constexpr uint16_t        defaultAudioSamples       = 4096;
+constexpr SDL_AudioFormat defaultAudioFormat        = AUDIO_F32SYS;
 
+struct AudioDevice
+{
+    SDL_AudioDeviceID deviceId;
+    SDL_AudioSpec     spec;
+};
+
+static AudioDevice audioDevice;
 
 AudioManager::AudioManager()  = default;
 AudioManager::~AudioManager() = default;
@@ -18,25 +31,45 @@ AudioManager &AudioManager::getInstance()
     return instance;
 }
 
+static void audioCallback(void *userdata, Uint8 *stream, int len) {}
+
 void AudioManager::init()
 {
     L_TAG("AudioManager::init");
 
+    // Verify SDL Audio was initialized
+    if (!(SDL_WasInit(SDL_INIT_AUDIO) & SDL_INIT_AUDIO)) L_THROW_RUNTIME("SDL_INIT_AUDIO was not initialized");
+
+    SDL_AudioSpec want, have;
+    SDL_memset(&want, 0, sizeof(want));
+    want.freq     = defaultAudioFrequency;
+    want.channels = defaultAudioChannel;
+    want.samples  = defaultAudioSamples;
+    want.format   = defaultAudioFormat;
+    want.callback = audioCallback;
+    want.userdata = &audioDevice;
+
+    // Open default audio device
+    SDL_AudioDeviceID devId = SDL_OpenAudioDevice(NULL, 0, &want, &have, audioDeviceAllowedChanges);
+    if (devId)
+    {
+        L_INFO("Using default audio device");
+        L_INFO("\t Frequency: {}Hz", have.freq);
+        L_INFO("\t Channels:  {}", have.channels);
+        L_INFO("\t Samples:   {}", have.samples);
+        L_INFO("\t Format:    0x{:x}", have.format);
+
+        audioDevice.deviceId = devId;
+        audioDevice.spec = have;
+    }
+    else
+    {
+        L_ERROR("Could not open default audio device");
+    }
 }
 
-void AudioManager::preUpdate()
-{
-    L_TAG("AudioManager::preUpdate");
+void AudioManager::preUpdate() { L_TAG("AudioManager::preUpdate"); }
 
-}
+void AudioManager::fixedUpdate(const time_ms &delta) { L_TAG("AudioManager::fixedUpdate"); }
 
-void AudioManager::fixedUpdate(const time_ms &delta) {
-    L_TAG("AudioManager::fixedUpdate");
-
-}
-
-void AudioManager::update(const time_ms &delta)
-{
-    L_TAG("AudioManager::update");
-    
-}
+void AudioManager::update(const time_ms &delta) { L_TAG("AudioManager::update"); }
