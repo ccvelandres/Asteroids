dc25e410dc651d53881e3f1f4f5b2488c2c626d3
ccvelandres <ccvelandres@gmail.com>
2022-12-04 12:23:12 +0800

    cleanup sample code

--- logic/src/main.cpp
+++ logic/src/main.cpp
@@ -41,12 +41,14 @@ class CameraObject : public Entity
 public:
     TransformComponent *transform;
     CameraComponent    *camera;
+    InputComponent     *inputComponent;
 
     CameraObject()
     {
         transform = &this->addComponent<TransformComponent>();
         camera =
             &this->addComponent<CameraComponent>(glm::vec2(1280.0f, 720.0f), CameraComponent::Projection::Perspective);
+        inputComponent = &this->addComponent<InputComponent>();
     }
 };
 
@@ -64,6 +66,7 @@ int main(int arc, char **argv)
 
     EntityManager *entityManager = Game::entityManager();
 
+    // Create crate entities
     std::array<Crate *, 8> crates = {&entityManager->addEntity<Crate>(glm::vec3(0.0f, 0.0f, 0.0f)),
                                      &entityManager->addEntity<Crate>(glm::vec3(2.5f, 0.0f, 0.0f)),
                                      &entityManager->addEntity<Crate>(glm::vec3(5.0f, 0.0f, 0.0f)),
@@ -73,66 +76,73 @@ int main(int arc, char **argv)
                                      &entityManager->addEntity<Crate>(glm::vec3(0.0f, 7.5f, 0.0f))};
 
     CameraObject &camObject = entityManager->addEntity<CameraObject>();
+    // reset position
     camObject.transform->setPosition(glm::vec3(0.0f, 0.0f, 0.0f))
         .setOrientation(TransformComponent::worldFront, TransformComponent::worldUp);
     camObject.camera->updateMatrix();
 
-    InputComponent *inputComponent = &camObject.addComponent<InputComponent>();
-    inputComponent->setListener(InputEventType::KeyDown, [&camObject](const InputEventType type, SDL_Event *ev) -> void {
-        L_INFO_RATE(32, "cameraInput KeyDown Event: {}", ev->key.keysym.sym);
-        CameraComponent    *cam       = &camObject.getComponent<CameraComponent>();
-        TransformComponent *transform = &camObject.getComponent<TransformComponent>();
-        switch (ev->key.keysym.sym)
-        {
-        case SDLK_w:
-            transform->translate(TransformComponent::worldFront, true);
-            break;
-        case SDLK_s:
-            transform->translate(-TransformComponent::worldFront, true);
-            break;
-        case SDLK_d:
-            transform->translate(TransformComponent::worldRight, true);
-            break;
-        case SDLK_a:
-            transform->translate(-TransformComponent::worldRight, true);
-            break;
-        case SDLK_LSHIFT:
-            transform->translate(TransformComponent::worldUp, true);
-            break;
-        case SDLK_LCTRL:
-            transform->translate(-TransformComponent::worldUp, true);
-            break;
-        case SDLK_KP_4:
-            transform->rotate(-5.0f, TransformComponent::worldUp);
-            break;
-        case SDLK_KP_6:
-            transform->rotate(5.0f, TransformComponent::worldUp);
-            break;
-        case SDLK_KP_2:
-            transform->rotate(-5.0f, TransformComponent::worldRight);
-            break;
-        case SDLK_KP_8:
-            transform->rotate(5.0f, TransformComponent::worldRight);
-            break;
-        }
-        cam->updateMatrix();
-        glm::quat q             = transform->getOrientation();
-        float     rotationAngle = std::acos(q.w) * 2;
-        float     td            = 1 / std::sin(rotationAngle / 2);
-        glm::vec3 rotationAxis(q.x * td, q.y * td, q.z * td);
+    // Set KeyDown input listener
+    camObject.inputComponent->setListener(
+        InputEventType::KeyDown,
+        [&camObject](const InputEventType type, SDL_Event *ev) -> void {
+            L_INFO_RATE(32, "cameraInput KeyDown Event: {}", ev->key.keysym.sym);
+            CameraComponent    *cam       = &camObject.getComponent<CameraComponent>();
+            TransformComponent *transform = &camObject.getComponent<TransformComponent>();
+            switch (ev->key.keysym.sym)
+            {
+            case SDLK_w:
+                transform->translate(TransformComponent::worldFront, true);
+                break;
+            case SDLK_s:
+                transform->translate(-TransformComponent::worldFront, true);
+                break;
+            case SDLK_d:
+                transform->translate(TransformComponent::worldRight, true);
+                break;
+            case SDLK_a:
+                transform->translate(-TransformComponent::worldRight, true);
+                break;
+            case SDLK_LSHIFT:
+                transform->translate(TransformComponent::worldUp, true);
+                break;
+            case SDLK_LCTRL:
+                transform->translate(-TransformComponent::worldUp, true);
+                break;
+            case SDLK_KP_4:
+                transform->rotate(-5.0f, TransformComponent::worldUp);
+                break;
+            case SDLK_KP_6:
+                transform->rotate(5.0f, TransformComponent::worldUp);
+                break;
+            case SDLK_KP_2:
+                transform->rotate(-5.0f, TransformComponent::worldRight);
+                break;
+            case SDLK_KP_8:
+                transform->rotate(5.0f, TransformComponent::worldRight);
+                break;
+            }
+            cam->updateMatrix();
+            glm::quat q             = transform->getOrientation();
+            float     rotationAngle = std::acos(q.w) * 2;
+            float     td            = 1 / std::sin(rotationAngle / 2);
+            glm::vec3 rotationAxis(q.x * td, q.y * td, q.z * td);
 
-        L_TRACE("position        {}", glm::to_string(transform->m_position));
-        L_TRACE("front:          {}", glm::to_string(transform->getFront()));
-        L_TRACE("right:          {}", glm::to_string(transform->getRight()));
-        L_TRACE("up:             {}", glm::to_string(transform->getUp()));
-        L_TRACE("rotationAxis:   {}", glm::to_string(rotationAxis));
-        L_TRACE("rotationAngle:  {}", rotationAngle);
-        L_TRACE("euler:          {}", glm::to_string(glm::degrees(glm::eulerAngles(transform->getOrientation()))));
-        L_TRACE("orientation:    {}", glm::to_string(transform->getOrientation()));
-        L_TRACE("orientation:    {}", glm::to_string(glm::mat4_cast(transform->getOrientation())));
-        L_TRACE("viewMatrix:     {}", glm::to_string(cam->getViewMatrix()));
-        L_TRACE("projection:     {}", glm::to_string(cam->getProjectionMatrix()));
-    });
+            L_TRACE("position        {}", glm::to_string(transform->m_position));
+            L_TRACE("front:          {}", glm::to_string(transform->getFront()));
+            L_TRACE("right:          {}", glm::to_string(transform->getRight()));
+            L_TRACE("up:             {}", glm::to_string(transform->getUp()));
+            L_TRACE("rotationAxis:   {}", glm::to_string(rotationAxis));
+            L_TRACE("rotationAngle:  {}", rotationAngle);
+            L_TRACE("euler:          {}", glm::to_string(glm::degrees(glm::eulerAngles(transform->getOrientation()))));
+            L_TRACE("orientation:    {}", glm::to_string(transform->getOrientation()));
+            L_TRACE("orientation:    {}", glm::to_string(glm::mat4_cast(transform->getOrientation())));
+            L_TRACE("viewMatrix:     {}", glm::to_string(cam->getViewMatrix()));
+            L_TRACE("projection:     {}", glm::to_string(cam->getProjectionMatrix()));
+        });
+
+    AudioComponent *audioComponent = &camObject.addComponent<AudioComponent>();
+
+    AudioClip *music = audioComponent->addAudioClip("menu.wav", AudioType::Global);
 
     game->startGameLoop();
 
