6c9b5f0c216310f66cc2b9de0b392e066065b494
ccvelandres <ccvelandres@gmail.com>
2023-03-02 16:11:51 +0800

    disable mesh defaults for now

--- core/include/core/assets/defaults/mesh-defaults.hpp
+++ core/include/core/assets/defaults/mesh-defaults.hpp
@@ -17,22 +17,25 @@
 
 namespace core::assets::defaults::mesh
 {
+    /** @todo: re-enable const defaults for some mesh types */
+    // inline static const Mesh xy_quad = {
+    //     .m_name = "defaults/xy_quad",
+    //     .m_hasTangents = false,
+    //     .m_hasBitangets = false,
+    //     .m_vertices = {
+    //         {{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}},
+    //         {{0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}},
+    //         {{1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}},
+    //         {{1.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}}
+    //     },
+    //     .m_indices = {
+    //         1, 0, 2, 1, 2, 3
+    //     }
+    // };
 
-    inline static const Mesh::MeshData xy_quad = {
-        .vertices = {
-            {{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},
-            {{0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
-            {{1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f}},
-            {{1.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}}
-        },
-        .indices = {
-            1, 0, 2, 1, 2, 3
-        }
-    };
-
-    const std::unordered_map<std::string, const Mesh::MeshData *> mesh_defaults = {
-        {"quad", &core::assets::defaults::mesh::xy_quad}
-    };
+    // const std::unordered_map<std::string, const Mesh::MeshData *> mesh_defaults = {
+    //     {"quad", &core::assets::defaults::mesh::xy_quad}
+    // };
 } // namespace core::assets::defaults::mesh
 
 /** @} endgroup Assets */
